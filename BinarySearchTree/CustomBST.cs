c3a09ddc1d0f8e66750489605607743c69d9d5aa
Akshay4955 <akshayshedge11@gmail.com>
2023-06-04 13:06:39 +0530

    Added: Code for BST add method

---
+++ BinarySearchTree/CustomBST.cs
@@ -0,0 +1,59 @@
+﻿namespace BinarySearchTree;
+
+public class CustomBST
+{
+    private class Node
+    {
+        public int Data;
+        public Node Left;
+        public Node Right;
+
+        public Node(int data)
+        {
+            Data = data;
+        }
+    }
+
+    private Node Root;
+
+    public void Add(int data)
+    {
+        Node newNode = new Node(data);
+
+        if (Root == null)
+        {
+            Root = newNode;
+            return;
+        }
+
+        Node tempNode = Root;
+
+        while (tempNode != null)
+        {
+            if (newNode.Data < tempNode.Data)
+            {
+                if (tempNode.Left == null)
+                {
+                    tempNode.Left = newNode;
+                    return;
+                }
+                tempNode = tempNode.Left;
+            }
+
+            else if (newNode.Data > tempNode.Data)
+            {
+                if (tempNode.Right == null)
+                {
+                    tempNode.Right = newNode;
+                    return;
+                }
+                tempNode = tempNode.Right;
+            }
+            else
+            {
+                Console.WriteLine($"{data} is already present in tree");
+                return;
+            }
+        }
+    }
+}
--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -5,5 +5,14 @@ internal class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to binary search tree...!!");
+        CustomBST myBST = new CustomBST();
+
+        myBST.Add(56);
+        myBST.Add(30);
+        myBST.Add(70);
+        myBST.Add(22);
+        myBST.Add(40);
+        myBST.Add(60);
+        myBST.Add(95);
     }
 }
